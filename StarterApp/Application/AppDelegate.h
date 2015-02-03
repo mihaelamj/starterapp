e2f2c4ff8629482254bfa8bba2c162ed90169bff
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2015-02-03 11:42:19 +0100

    initial commit

---
+++ StarterApp/Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StarterApp
+//
+//  Created by Mihaela Mihaljević Jakić on 24/01/15.
+//  Copyright (c) 2015 Mihaela Mihaljević Jakić. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StarterApp/Supporting Files/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StarterApp
+//
+//  Created by Mihaela Mihaljević Jakić on 24/01/15.
+//  Copyright (c) 2015 Mihaela Mihaljević Jakić. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
